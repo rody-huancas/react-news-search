a8ff69a398584525ea4b134f73c0758e38d54911
Rody Huancas <rodyhuancas.04@gmail.com>
2023-09-10 10:48:05 -0500

    Add: Form

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Container, Grid, Typography } from "@mui/material";
+import { Formulario } from "./components/Formulario";
+
+const App = () => {
+  return (
+    <>
+      <Container>
+        <header>
+          <Typography align="center" marginY={5} component="h1" variant="h3">
+            Buscador de Noticias
+          </Typography>
+        </header>
+
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={12} md={6} lg={4}>
+            <Formulario />
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+};
+
+export default App;
